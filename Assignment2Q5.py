1d1210aab9360c9a643f7e08aaad195ea8866569
YASH GAIKWAD <gaikwady449@gmail.com>
2026-01-19 15:22:42 +0530

    Update Assignment2Q5.py

--- Assignment2Q5.py
+++ Assignment2Q5.py
@@ -1,4 +1,4 @@
-#write a program which accept  one number and print even numbers till that number
+#write a program which accept  one number and print odd numbers till that number
 
 def even(No):
      for i in range(10):
